c21e552d0fade226fbaac15a11e4c4ac542e88fc
nzaoo <nzao1327@gmail.com>
2025-06-30 13:17:09 +0700

    update more file

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { HeaderProps } from '@/types'
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
