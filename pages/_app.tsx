4eccba6514c629ad50a709057207b654e1c7ef86
nzaoo <nzao1327@gmail.com>
2025-07-16 08:01:59 +0700

    Update project files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -85,6 +85,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5" />
         <meta name="theme-color" content="#fbbf24" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="NZaoCard" />
